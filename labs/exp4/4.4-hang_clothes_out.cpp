b192a674fb2a45568dde3ce1d8a0f4be4438c52e
RabbitWhite1 <895650485@qq.com>
2020-12-12 17:10:34 +0800

    exp4

--- labs/exp4/4.4-hang_clothes_out.cpp
+++ labs/exp4/4.4-hang_clothes_out.cpp
@@ -1,3 +1,48 @@
+#ifndef STUPID
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    vector<int> clothes(n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &clothes[i]);
+    }
+
+    sort(clothes.begin(), clothes.end(), less<int>());
+
+    int left = 0, right = clothes[n-1] - clothes[0];
+    int target = n - m;
+    // 借鉴学长的思路, 采用二分策略 (https://github.com/lyc0930/Online-Judge/tree/master/ItA/E4-EX)
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        int last_x = clothes[0];
+        int count = 1;
+
+        for (int i = 1; i < n && count < target; i++) {
+            if (clothes[i] >= last_x + mid) {
+                count++;
+                last_x = clothes[i];
+            }
+        }
+
+        if (count >= target) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    printf("%d\n", right);
+
+}
+
+#else
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -17,8 +62,6 @@ public:
 };
 
 using ClothPair = pair<int, int>;
-
-
 
 int main() {
     int n, m;
@@ -44,46 +87,32 @@ int main() {
         distances.push(ClothPair(i, i+1));
     }
     clothes[n-1].next_idx = -1;
-    clothes[n-1].prev_idx = n-1;
+    clothes[n-1].prev_idx = n-2;
     
     for (int i = 0; i < m; i++) {
         auto nearest = distances.top();
         distances.pop();
-        while (true) {
-            auto left = nearest.first;
-            auto right = nearest.second;
-            if (clothes[left].x == -1) {
-                nearest = distances.top();
-                distances.pop();
-            }
-            else if (clothes[right].x == -1) {
-                nearest = distances.top();
-                distances.pop();
-            }
-            else break;
+        while (clothes[nearest.first].x == -1 || clothes[nearest.second].x == -1) {
+            nearest = distances.top();
+            distances.pop();
         }
         auto left = nearest.first;
         auto right = nearest.second;
 
         auto to_remove = left;
-        if (clothes[left].prev_idx != -1) {
-            if (clothes[right].next_idx == -1) to_remove = left;
-            else {
-                // if left isn't begin and right isn't last
-                auto leftleft = clothes[left].prev_idx;
-                auto rightright = clothes[right].next_idx;
-                if (clothes[right].x - clothes[leftleft].x < clothes[rightright].x - clothes[left].x) {
-                    // remove right
-                    to_remove = right;
-                }
-                else {
-                    // remove left
-                    to_remove = left;
-                }
-            }
+        if (clothes[left].prev_idx == -1) {
+            to_remove = right;
         }
-        else to_remove = right;
-
+        else if (clothes[right].next_idx == -1) to_remove = left;
+        else {
+            // if left isn't begin and right isn't last
+            auto leftleft = clothes[left].prev_idx;
+            auto rightright = clothes[right].next_idx;
+            int ifleft = min(clothes[right].x - clothes[leftleft].x, clothes[rightright].x - clothes[right].x);
+            int ifright = min(clothes[rightright].x - clothes[left].x, clothes[right].x - clothes[leftleft].x);
+            if (ifleft < ifright) to_remove = right;
+            else to_remove = left;
+        }
 
         distances.push(ClothPair(clothes[to_remove].prev_idx, clothes[to_remove].next_idx));
 
@@ -94,130 +123,12 @@ int main() {
 
     auto nearest = distances.top();
     distances.pop();
-    while (true) {
-        auto left = nearest.first;
-        auto right = nearest.second;
-        if (clothes[left].x == -1) {
-            nearest = distances.top();
-            distances.pop();
-        }
-        else if (clothes[right].x == -1) {
-            nearest = distances.top();
-            distances.pop();
-        }
-        else break;
+    while (clothes[nearest.first].x == -1 || clothes[nearest.second].x == -1) {
+        nearest = distances.top();
+        distances.pop();
     }
 
     printf("%d\n", clothes[nearest.second].x - clothes[nearest.first].x);
 
 }
-
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
-// #include <list>
-// #include <queue>
-// #include <unordered_set>
-
-// using namespace std;
-
-// class Cloth {
-// public:
-//     int x;
-//     int next_idx;
-//     int prev_idx;
-//     Cloth(int x): x(x) {}
-// };
-
-// using It = list<int>::iterator;
-// using RIt = list<int>::reverse_iterator;
-// using ClothPair = pair<It, It>;
-
-// int main() {
-//     int n, m;
-//     scanf("%d %d", &n, &m);
-
-//     vector<int> clothes;
-
-//     for (int i = 0; i < n; i++) {
-//         int x;
-//         scanf("%d", &x);
-//         clothes.push_back(x);
-//     }
-
-//     sort(clothes.begin(), clothes.end(), [](const int &x, const int &y){ return x < y; });
-
-//     list<int> clothes_list;
-//     auto cmp = [](const ClothPair &x, const ClothPair &y) {
-//         return *x.first - *x.first > *y.second - *y.first;
-//     };
-//     priority_queue<ClothPair, vector<ClothPair>, decltype(cmp)> distances(cmp);
-//     set<It> remains;
-
-//     clothes_list.push_back(clothes[0]);
-//     for (int i = 1; i < n; i++) {
-//         clothes_list.push_back(clothes[i]);
-//         distances.push(ClothPair(prev(prev(clothes_list.end())), prev(clothes_list.end())));
-//         remains.insert(prev(clothes_list.end()));
-//     }
-    
-//     for (int i = 0; i < m; i++) {
-//         auto nearest = distances.top();
-//         distances.pop();
-//         while (true) {
-//             auto left = nearest.first;
-//             auto right = nearest.second;
-//             if (remains.find(left) == remains.end()) {
-//                 nearest = distances.top();
-//                 distances.pop();
-//             }
-//             else if (remains.find(right) == remains.end()) {
-//                 nearest = distances.top();
-//                 distances.pop();
-//             }
-//             else break;
-//         }
-//         auto left = nearest.first;
-//         auto right = nearest.second;
-
-//         auto to_remove = left;
-//         if (left != clothes_list.begin()) {
-//             // if left isn't begin
-//             if (right == prev(clothes_list.end())) {
-//                 // if right is the last
-//                 to_remove = right;
-//             }
-//             else {
-//                 // if left isn't begin and right isn't last
-//                 auto leftleft = prev(left);
-//                 auto rightright = next(right);
-//                 if (*right - *leftleft < *rightright - *left) {
-//                     to_remove = right;
-//                 }
-//                 else to_remove = left;
-//             }
-//         }
-        
-//         remains.erase(to_remove);
-//         clothes_list.erase(to_remove);
-//     }
-
-//     auto nearest = distances.top();
-//     distances.pop();
-//     while (true) {
-//         auto left = nearest.first;
-//         auto right = nearest.second;
-//         if (remains.find(left) == remains.end()) {
-//             nearest = distances.top();
-//             distances.pop();
-//         }
-//         else if (remains.find(right) == remains.end()) {
-//             nearest = distances.top();
-//             distances.pop();
-//         }
-//         else break;
-//     }
-
-//     printf("%d\n", *nearest.second - *nearest.first);
-
-// }
+#endif
